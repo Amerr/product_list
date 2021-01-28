125cd07e74eb1095d2e3fd17bdb417213875b1b1
amerr <amerrnath.21@gmail.com>
2021-01-28 12:16:28 +0530

    Basic Layout structure

---
+++ src/components/ProductContainer.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+import ProductList from './ProductList';
+import ProductFilter from './ProductFilter';
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: row;
+  height: 80vh;
+`;
+
+const FilterWrapper = styled.div`
+  flex-basis: 30%;
+  border-right: 2px solid #ccc;
+`;
+
+const ProductListWrapper = styled.div`
+  flex-basis: 70%;
+`;
+
+const ProductContainer = () => {
+  return (
+    <Wrapper>
+      <FilterWrapper>
+        <ProductFilter />
+      </FilterWrapper>
+      <ProductListWrapper>
+        <ProductList />
+      </ProductListWrapper>
+    </Wrapper>
+  );
+};
+
+export default ProductContainer;
