d32f030ad43eea62ef3a1ae5e676ab82b59fbe44
Amerrnath Murugan <amerrnath.21@gmail.com>
2021-01-28 20:18:56 +0530

    Product Item UI (#3)
    - Format currency
    - Caluculate for 100ML
    - Design changes for Item

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,5 +1,38 @@
+import { useSelector } from 'react-redux';
+import { v4 as uuid } from 'uuid';
+import styled from 'styled-components';
+import { RootState } from '../redux/reducers';
+import ProductItem from './ProductItem';
+
+const ListWrapper = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  flex-basis: 30%;
+
+  @media (max-width: 768px) {
+    flex-basis: 45%;
+  }
+`;
+
 const ProductList = () => {
-  return <div>List of Products </div>;
+  const items = useSelector((state: RootState) => state.product);
+  return (
+    <div>
+      <ListWrapper>
+        {items.map((item) => (
+          <ListItem key={uuid()}>
+            <ProductItem data={item} />
+          </ListItem>
+        ))}
+      </ListWrapper>
+    </div>
+  );
 };
 
 export default ProductList;
