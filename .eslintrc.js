d59e516e2b39f075f2a4e24b174c788747b67296
amerr <amerrnath.21@gmail.com>
2021-01-28 11:10:26 +0530

    Update Eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,41 @@
 module.exports = {
-    root: true, // Make sure eslint picks up the config at the root of the directory
-    parserOptions: {
-        ecmaVersion: 2020, // Use the latest ecmascript standard
-        sourceType: 'module', // Allows using import/export statements
-        ecmaFeatures: {
-            jsx: true // Enable JSX since we're using React
-        }
-    },
-    settings: {
-        react: {
-            version: 'detect' // Automatically detect the react version
-        }
-    },
-    env: {
-        browser: true, // Enables browser globals like window and document
-        amd: true, // Enables require() and define() as global variables as per the amd spec.
-        node: true // Enables Node.js global variables and Node.js scoping.
-    },
     extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:jsx-a11y/recommended',
-        'plugin:prettier/recommended' // Make this the last element so prettier config overrides other formatting rules
+        'airbnb-typescript',
+        'airbnb/hooks',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:jest/recommended',
+        'prettier',
+        'prettier/react',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended'
     ],
+    plugins: ['react', '@typescript-eslint', 'jest'],
+    env: {
+        browser: true,
+        es6: true,
+        jest: true
+    },
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        project: './tsconfig.json'
+    },
     rules: {
-        'prettier/prettier': ['error', {}, { usePrettierrc: true }] // Use our .prettierrc file as source
+        "react/react-in-jsx-scope": "off",
+        'linebreak-style': 'off',
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto'
+            }
+        ]
     }
 };
